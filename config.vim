7691465621eb35bc5ccc4d9a5c7675050ae8c7e8
kousha ghodsizad <kousha.ghodsizad@gmail.com>
2020-06-26 14:54:36 +0300

    try to fix split

--- config.vim
+++ config.vim
@@ -41,7 +41,7 @@ let g:auto_ctags_directory_list = ['.git', '.svn']
 let g:auto_ctags_tags_name = 'tags'
 let g:auto_ctags_tags_args = ['--tag-relative=yes', '--recurse=yes', '--sort=yes']
 let g:auto_ctags_filetype_mode = 1
-
+set tags=tags;/
 "-------------------------------CTRLP----------------------------------"
 let g:ctrlp_custom_ignore = 'git|node_modules\|DS_Store\'
 
--- install.sh
+++ install.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
+rm -rvf  ~/.vimrc  ~/.gvimrc ~/.vim
 mkdir ~/.vim
 
 ./fonts/install.sh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 brew install cmake macvim python mono go nodejs ctags the_silver_searcher php-cs-fixer
 
-rm -rvf  ~/.vimrc  ~/.gvimrc ~/.vim
-mkdir .vim
-
 ln -s ~/vim/vimrc.vim ~/.vimrc
 ln -s ~/vim/gvimrc.vim ~/.gvimrc
-ln -s ~/vim/bundle ~/.vim/bundle
+ln -s ~/vim/fonts ~/.vim/fonts
 ln -s ~/vim/colors ~/.vim/colors
 ln -s ~/vim/autoload ~/.vim/autoload
 ln -s ~/vim/snippets ~/.vim/snippets
 
 vim +BundleClean +PluginInstall +qall
 
-python3 ~/vim/bundle/YouCompleteMe/install.py --all
+python3 ~/.vim/bundle/YouCompleteMe/install.py --all
